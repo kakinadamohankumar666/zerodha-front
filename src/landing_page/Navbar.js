908b438d838d67272cab2503518167e15446cc0a
mohan kumar <varshit666@gmail.com>
2025-07-12 09:19:42 +0530

    Fix: sign up

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -17,7 +17,7 @@ function Navbar() {
       <form class="d-flex" role="search">
         <ul class="navbar-nav  mb-lg-0">
         <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/signup">Signup</Link>
+          <Link class="nav-link active" aria-current="page" to="/">Signup</Link>
         </li>
         <li class="nav-item">
           <Link class="nav-link active" to="/about">About</Link>
